4fccb9d090bf97218e14a04f81b2d8e30fcede52
Jonny Bylsma <jmbylsma@gmail.com>
2019-02-23 21:42:26 -0500

    Last.fm track querying working for single user

--- scripts/background.js
+++ scripts/background.js
@@ -6,16 +6,99 @@ const DEFAULT_SETTINGS = {
     'users': ''
 };
 
-const UPDATE_INTERVAL= 120;
+const LASTFM_API_URL = 'http://ws.audioscrobbler.com/2.0/';
 
 // Populate missing default settings on startup.
-let settings = browser.storage.local.get();
-
-settings.then((settings) => {
+const gettingStoredSettings = browser.storage.local.get();
+gettingStoredSettings.then((settings) => {
     for (let prop in DEFAULT_SETTINGS) {
         if (!settings[prop]) {
             browser.storage.local.set(DEFAULT_SETTINGS);
             break;
         }
+    }
+});
+
+/**
+ * Wrapper for sending successful messages.
+ *
+ * @param data Any relevant data for the successful message.
+ */
+function sendSuccess(data = {}) {
+    browser.runtime.sendMessage({
+        status: 'success',
+        data: data,
+    });
+}
+
+/**
+ * Wrapper for sending error messages.
+ *
+ * @param name The shortname of the error.
+ * @param data Any relevant data for the error.
+ */
+function sendError(name, data = {}) {
+    browser.runtime.sendMessage({
+        status: 'error',
+        name: name,
+        data: data,
+    });
+}
+
+
+/**
+ * Get users' recent tracks.
+ */
+function getRecentTracks(storedSettings) {
+    // TODO: How to handle settings changes.
+    // TODO: Only check every X minutes, cache results.
+    // TODO: Locking system?
+    // TODO: Handle multiple users
+
+    if (storedSettings['apiKey'].trim().length === 0) {
+        sendError('settingsApiKey');
+        return;
+    }
+
+    if (storedSettings['users'].trim().length === 0) {
+        sendError('settingsUsers');
+        return;
+    }
+
+    let url = LASTFM_API_URL +
+        '?method=user.getrecenttracks' +
+        '&api_key=' + encodeURIComponent(storedSettings['apiKey']) +
+        '&user=' + encodeURIComponent(storedSettings['users']) +
+        '&limit=' + encodeURIComponent(storedSettings['fetchLimit']) +
+        '&format=json';
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4) {
+            if (this.status === 200) {
+                sendSuccess(JSON.parse(xhr.response));
+            }
+            else {
+                sendError('apiFail', xhr);
+            }
+        }
+    };
+    xhr.onabort = xhr.onerror = function() {
+        sendError('xhrFail', xhr);
+    };
+    xhr.send();
+}
+
+// Listen for messages from the options and newtab pages.
+browser.runtime.onMessage.addListener((action) => {
+    switch (action) {
+        case 'getRecentTracks':
+            const gettingStoredSettings = browser.storage.local.get();
+            gettingStoredSettings.then(getRecentTracks);
+            break;
+
+        default:
+            console.error('No action found: %s', action);
     }
 });
--- scripts/newtab.js
+++ scripts/newtab.js
@@ -1,1 +1,45 @@
 'use strict';
+
+let main = document.querySelector('main');
+
+// Plain-text descriptions for possible error messages.
+const ERROR_DESCRIPTIONS = {
+    settingsApiKey: 'The Last.fm API Key has not been set.',
+    settingsUsers: 'The Last.fm users have not been set.',
+    apiFail: 'The connection failed to the Last.fm API.',
+    xhrFail: 'Could not create a connection to Last.fm API.',
+};
+
+// Listen for messages from background Javascript.
+browser.runtime.onMessage.addListener((message) => {
+    main.textContent = '';
+
+    if (message.status === 'success') {
+        // TODO: Format and output data.
+        console.log('success');
+        console.log(message);
+        console.log(message.data);
+    }
+    else {
+        console.log('error');
+        console.log(message);
+        let errorDescription = ERROR_DESCRIPTIONS[message.name];
+
+        // Handle missing settings.
+        if (message.name.startsWith('settings')) {
+            let optionsLink = document.createElement('a');
+            optionsLink.textContent = errorDescription;
+            optionsLink.setAttribute('href', '');
+
+            optionsLink.addEventListener('click', function() {
+                browser.runtime.openOptionsPage();
+                return false;
+            });
+
+            main.appendChild(optionsLink);
+        }
+    }
+});
+
+// TODO: Run constantly, not just on load.
+browser.runtime.sendMessage('getRecentTracks');
