1afe474f9d7755cc06efc61861e7c32170aecdf7
Jonny Bylsma <jbylsma@quotient-inc.com>
2020-07-09 09:52:51 -0400

    Configure NPM and ESLint with Standard

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    webextensions: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 11
+  },
+  rules: {
+  }
+}
